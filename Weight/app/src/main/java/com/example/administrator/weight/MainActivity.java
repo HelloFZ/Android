cdde64fd3790a6b7dcba154cef8107c557f7a47b
HelloFZ <869640364@qq.com>
2017-03-15 04:02:20 +0800

    Signed-off-by: HelloFZ <869640364@qq.com>

---
+++ Weight/app/src/main/java/com/example/administrator/weight/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.administrator.weight;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    public static String sex = "M";
+    private String str_height;
+    private EditText et;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button b1 = (Button) findViewById(R.id.btn_calculate);
+        et = (EditText) findViewById(R.id.et_height);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                    /*取得选择的性别*/
+                RadioGroup rg = (RadioGroup) findViewById(R.id.rg_sex);
+                str_height = et.getText().toString();
+                if ("".equals(str_height)) {
+                    Toast.makeText(MainActivity.this, "体重不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                double height = Double.parseDouble(str_height);
+                rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(RadioGroup arg0, int checkedId) {
+                        // TODO Auto-generated method stub
+                        if (checkedId == R.id.rb_male) {
+                                sex = "M";
+                        } else {
+                            sex = "F";
+                        }
+                        Log.i("zhouweizhi<<<<", sex);
+                    }
+                });
+                    /*new一个Intent对象，并指定class*/
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, result.class);
+                    /*new一个Bundle对象，并将要传递的数据传入*/
+                Bundle bundle = new Bundle();
+                bundle.putDouble("height", height);
+                bundle.putString("sex", sex);
+                    /*将Bundle对象assign给Intent*/
+                intent.putExtras(bundle);
+	    	        /*调用Activity*/
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+
+
+}
